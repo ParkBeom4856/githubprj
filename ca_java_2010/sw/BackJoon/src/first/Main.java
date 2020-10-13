b32742b895f41c2a11953cd0b9d6709780696af2
seoulbum4856 <seoulbum4856@naver.com>
2020-10-13 12:37:12 +0900

    "add GitHub"

---
+++ ca_java_2010/sw/BackJoon/src/first/Main.java
@@ -0,0 +1,12 @@
+package first;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("\\    /\"");
+		System.out.println(" )   ( ')");
+		System.out.println("(   /  )");
+		System.out.println("\\(__) |");
+	}
+
+}
